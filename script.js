0c49f9b15d348f04b89f3ec801f4d224245da47e
swartztz <122939910+swartztz@users.noreply.github.com>
2025-03-05 23:31:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function storeData() {
     let name = document.getElementById("nameInput").value.trim();
     let email = document.getElementById("emailInput").value.trim();
     let address = document.getElementById("addressInput").value.trim();
+    let zc = document.getElementById("zcInput").value.trim();
 
     if (name === "" || email === "" || address === "") {
         alert("Please fill in all fields!");
@@ -10,6 +11,7 @@ function storeData() {
         localStorage.setItem("name", name);
         localStorage.setItem("email", email);
         localStorage.setItem("address", address);
+        localStorage.setItem("zc", zc);
         
         // Redirect to output.html
         window.location.href = "output.html";
